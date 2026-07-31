29185f829f852d028c44a7cb07f8b2e3bdc4d198
Supreety Jha <supreetyjha@gmail.com>
2026-07-31 18:22:32 +0530

    Add env

--- web/src/api/client.js
+++ web/src/api/client.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
-
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 export async function encryptCaesar(text, shift) {
   const res = await fetch(`${BASE_URL}/classical/caesar/encrypt`, {
     method: "POST",
